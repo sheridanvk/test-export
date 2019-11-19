02c62a9392eb7791033836c7e974354870592805
Glitch (build-a-profile) <none>
2019-11-19 11:39:24 +0000

    🌃🐏 Updated with Glitch

--- script.js
+++ script.js
@@ -2,4 +2,15 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("hi");
+document.getElementById("more-section").addEventListener("toggle", event => {
+  let text;
+  if (event.target.open) {
+    text = "less about me";
+  } else {
+    text = "more about me";
+  }
+  document.getElementById("more-button").innerHTML = text;
+});
+
+// for debugging; comment out when not using
+// document.getElementsByTagName("details")[0].open = true;
